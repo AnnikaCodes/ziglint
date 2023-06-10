50898b69b42a42a6e1d18531e7a00556272860ba
Annika <annika@worldbrightening.net>
2023-06-09 18:15:49 -0700

    Try to fix the action

--- src/main.zig
+++ src/main.zig
@@ -67,11 +67,7 @@ pub fn main() anyerror!void {
         if (deinit_status == .leak) @panic("MEMORY LEAK");
     }
 
-    const files = if (res.positionals.len > 0) res.positionals else cwd: {
-        const result = try std.process.getCwdAlloc(allocator);
-        defer allocator.free(result);
-        break :cwd &[_][]const u8{result};
-    };
+    const files = if (res.positionals.len > 0) res.positionals else &[_][]const u8 { "." };
     for (files) |file| {
         var analyzer = try get_analyzer(file, allocator);
 
