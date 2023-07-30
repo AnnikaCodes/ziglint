508df2bd7302e05ac641dadb32608fa5f7e5da00
Annika <annika@worldbrightening.net>
2023-07-29 21:20:23 -0700

    stuff is a mess

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,7 @@
 
 const std = @import("std");
 const builtin = @import("builtin");
-const parallel = @import("./parallel.zig");
+const ParallelLinter = @import("./parallel_lint.zig").ParallelLinter;
 
 // const git_revision = @import("gitrev").revision;
 const analysis = @import("./analysis.zig");
@@ -28,9 +28,6 @@ const fields = std.meta.fields(analysis.ASTAnalyzer);
 
 // for sorting
 // TODO figure out the idiomatic way to sort in zig
-fn less_than(_: @TypeOf(.{}), a: analysis.SourceCodeFault, b: analysis.SourceCodeFault) bool {
-    return a.line_number < b.line_number;
-}
 
 // a comptime BufferedWriter is for some reason not working on Windows
 // we could pass around a writer, but for now I've been lazy and just got a writer every time
@@ -145,7 +142,7 @@ const JSONConfiguration = struct {
     }
 };
 
-const Configuration = struct {
+pub const Configuration = struct {
     max_line_length: ?SeverityLevelPlusConfig(u32) = null,
     check_format: ?SeverityLevel = null,
     dupe_import: ?SeverityLevel = null,
@@ -417,7 +414,14 @@ pub fn main() anyerror!void {
             }
         }
 
-        error_count += try lint(file, allocator, analyzer, &ignore_tracker, &seen, config, true);
+        var linter = ParallelLinter.init(
+            arena_allocator,
+            allocator,
+            &analyzer,
+            &config,
+            &ignore_tracker,
+        );
+        error_count += try linter.run(file);
     }
 
     if (error_count >= 256) {
@@ -509,251 +513,3 @@ fn find_file(alloc: std.mem.Allocator, file_name: []const u8, search_name: []con
     }
     return null;
 }
-
-// Lints a file.
-//
-// If it's a directory, recursively search it for .zig files and lint them.
-//
-// Returns the number of faults found.
-fn lint(
-    file_name: []const u8,
-    alloc: std.mem.Allocator,
-    analyzer: analysis.ASTAnalyzer,
-    ignore_tracker: *const IgnoreTracker,
-    seen: *std.StringHashMap(void),
-    config: Configuration,
-    is_top_level: bool,
-) anyerror!u64 {
-    // we need the full, not relative, path to make sure we avoid symlink loops
-    const real_path = try std.fs.realpathAlloc(alloc, file_name);
-    if (seen.contains(real_path)) {
-        // we need to free the `real_path` memory here since we're not adding it to the hashmap
-        alloc.free(real_path);
-        return 0; // no faults, since we already checked this path
-    } else {
-        try seen.put(real_path, {});
-    }
-
-    const file = std.fs.cwd().openFile(file_name, .{}) catch |err| {
-        switch (err) {
-            error.AccessDenied => try stderr_print("error: access denied: '{s}'", .{file_name}),
-            error.DeviceBusy => try stderr_print("error: device busy: '{s}'", .{file_name}),
-            error.FileNotFound => try stderr_print("error: file not found: '{s}'", .{file_name}),
-            error.FileTooBig => try stderr_print("error: file too big: '{s}'", .{file_name}),
-
-            error.SymLinkLoop => {
-                // symlink loops should be caught by our hashmap of seen files
-                // if not, we have a problem, so let's check
-                if (!seen.contains(real_path)) {
-                    try stderr_print(
-                        "error: couldn't open '{s}' due to a symlink loop, " ++
-                            "but it still hasn't been linted (full path: {s})",
-                        .{ file_name, real_path },
-                    );
-                }
-            },
-
-            // Windows can't open a directory with openFile, apparently.
-            error.IsDir => {
-                return try walk_directory(file_name, alloc, analyzer, ignore_tracker, seen, config);
-            },
-
-            else => try stderr_print("error: couldn't open '{s}': {}", .{ file_name, err }),
-        }
-
-        // exit the program if the *user* specified an inaccessible file;
-        // otherwise, just skip it
-        if (is_top_level) {
-            std.os.exit(1);
-        } else {
-            return 0;
-        }
-    };
-    defer file.close();
-
-    const metadata = try file.metadata(); // TODO: is .stat() faster?
-    const kind = metadata.kind();
-    var error_count: u64 = 0;
-    switch (kind) {
-        .file => {
-            if (!is_top_level) {
-                // not a Zig file + not directly specified by the user
-                if (!std.mem.endsWith(u8, file_name, ".zig")) return error_count;
-                // ignored by a .gitignore
-                if (try ignore_tracker.is_ignored(file_name)) return error_count;
-            }
-
-            error_count += try lint_file(
-                alloc,
-                metadata,
-                file,
-                file_name,
-                analyzer,
-                config,
-            );
-        },
-        .directory => {
-            error_count += try walk_directory(file_name, alloc, analyzer, ignore_tracker, seen, config);
-        },
-        else => {
-            try stderr_print(
-                "ignoring '{s}', which is not a file or directory, but a(n) {}.",
-                .{ file_name, kind },
-            );
-        },
-    }
-    return error_count;
-}
-
-fn lint_file(
-    alloc: std.mem.Allocator,
-    metadata: std.fs.File.Metadata,
-    file: std.fs.File,
-    file_name: []const u8,
-    analyzer: analysis.ASTAnalyzer,
-    config: Configuration,
-) !u64 {
-    // lint it
-    var error_count: u64 = 0;
-    const contents = try alloc.allocSentinel(u8, metadata.size(), 0);
-    defer alloc.free(contents);
-    _ = file.readAll(contents) catch |err| {
-        try stderr_print("error: couldn't read '{s}': {}", .{ file_name, err });
-        return error_count;
-    };
-
-    var ast = try std.zig.Ast.parse(alloc, contents, .zig);
-    defer ast.deinit(alloc);
-    var faults = try analyzer.analyze(alloc, file_name, ast);
-    defer faults.deinit();
-
-    // TODO just return faults.items
-
-    const sorted_faults = std.sort.insertion(analysis.SourceCodeFault, faults.faults.items, .{}, less_than);
-    _ = sorted_faults;
-    const stdout = std.io.getStdOut();
-    const stdout_writer = stdout.writer();
-
-    const use_color: bool = stdout.supportsAnsiEscapeCodes();
-    const bold_text: []const u8 = if (use_color) "\x1b[1m" else "";
-    const red_text: []const u8 = if (use_color) "\x1b[31m" else "";
-    const yellow_text: []const u8 = if (use_color) "\x1b[33m" else "";
-
-    // currently not used but makes for a nice highlight!
-    // const bold_magenta: []const u8 = if (use_color) "\x1b[1;35m" else "";
-    const end_text_fmt: []const u8 = if (use_color) "\x1b[0m" else "";
-    for (faults.faults.items) |fault| {
-        try stdout_writer.print("{s}{s}:{}:{}{s}: ", .{
-            bold_text,
-            file_name,
-            fault.line_number,
-            fault.column_number,
-            end_text_fmt,
-        });
-        var warning = false;
-        var fault_formatting = red_text;
-        switch (fault.fault_type) {
-            .LineTooLong => |len| {
-                if (config.max_line_length != null and config.max_line_length.?.severity == .Warning) {
-                    warning = true;
-                    fault_formatting = yellow_text;
-                }
-
-                try stdout_writer.print(
-                    "line is {s}{} characters long{s}; the maximum is {}",
-                    .{ fault_formatting, len, end_text_fmt, analyzer.max_line_length },
-                );
-            },
-            .BannedCommentPhrase => |phrase_info| {
-                if (phrase_info.severity_level == .Warning) {
-                    warning = true;
-                    fault_formatting = yellow_text;
-                }
-
-                try stdout_writer.print(
-                    "comment includes banned phrase '{s}{s}{s}':\n    {s}=> {s}{s}",
-                    .{
-                        fault_formatting, phrase_info.phrase, end_text_fmt,
-                        fault_formatting, end_text_fmt,       phrase_info.comment,
-                    },
-                );
-            },
-            .DupeImport => |name| {
-                // TODO: can we do some shenanigans with @field to make this if not have to be in every switch?
-                if (config.dupe_import == .Warning) {
-                    warning = true;
-                    fault_formatting = yellow_text;
-                }
-
-                try stdout_writer.print(
-                    "found {s}duplicate import{s} of {s}",
-                    .{ fault_formatting, end_text_fmt, name },
-                );
-            },
-            .FileAsStruct => |capitalize| {
-                if (config.file_as_struct == .Warning) {
-                    warning = true;
-                    fault_formatting = yellow_text;
-                }
-
-                if (capitalize) {
-                    try stdout_writer.print(
-                        "found top level fields, file name should be {s}capitalized{s}",
-                        .{ fault_formatting, end_text_fmt },
-                    );
-                } else {
-                    try stdout_writer.print(
-                        "found no top level fields, file name should be {s}lowercase{s}",
-                        .{ fault_formatting, end_text_fmt },
-                    );
-                }
-            },
-            .ImproperlyFormatted => {
-                if (config.check_format == .Warning) {
-                    warning = true;
-                    fault_formatting = yellow_text;
-                }
-                try stdout_writer.print(
-                    "the file is {s}improperly formatted{s}; try using `zig fmt` to fix it",
-                    .{ fault_formatting, end_text_fmt },
-                );
-            },
-            .ASTError => {
-                try stdout_writer.print("Zig's code parser detected an error: {s}", .{red_text});
-                try ast.renderError(fault.ast_error.?, stdout_writer);
-                try stdout_writer.print("{s}", .{end_text_fmt});
-            },
-        }
-        try stdout_writer.writeAll("\n");
-
-        if (!warning) error_count += 1;
-    }
-    return error_count;
-}
-// iterate over a directory and lint
-// returns # of faults found
-fn walk_directory(
-    file_name: []const u8,
-    alloc: std.mem.Allocator,
-    analyzer: analysis.ASTAnalyzer,
-    ignore_tracker: *const IgnoreTracker,
-    seen: *std.StringHashMap(void),
-    config: Configuration,
-) !u64 {
-    // todo: is walker faster?
-    var dir = try std.fs.cwd().openIterableDir(file_name, .{});
-    defer dir.close();
-
-    var iterable = dir.iterate();
-    var entry = try iterable.next();
-    var error_count: u64 = 0;
-    while (entry != null) : (entry = try iterable.next()) {
-        const full_name = try std.fs.path.join(alloc, &[_][]const u8{ file_name, entry.?.name });
-        defer alloc.free(full_name);
-        error_count += try lint(full_name, alloc, analyzer, ignore_tracker, seen, config, false);
-    }
-    return error_count;
-}
-
-// TODO: more lints! (import order, cyclomatic complexity)
-// TODO: support disabling the linter for a line/region of code
---
+++ src/parallel_lint.zig
@@ -0,0 +1,373 @@
+//! Parallelization code.
+//!
+//! vvv the below stuff is wrong vvv
+//! The current architecture is that the main process iterates over the director(ies) and finds
+//! the files that need to be linted.
+//! It puts them into a queue, which each of the worker threads pulls from.
+//! Each worker thread grabs files from the queue and lints them, incrementing its own local fault counter.
+//! (These worker threads also print the faults in the files as they lint them.)
+//!
+//! When the files are all in the queue, the main process will put some sort of signal on the queue
+//! that will instruct the worker threads to add their fault counters to the shared fault counter (protected by a RwLock).
+//! ASTAnalyzers are not mutated during analysis, so one *const ASTAnalyzer pointer can be shared between threads.
+
+const std = @import("std");
+
+const analysis = @import("./analysis.zig");
+const main = @import("./main.zig");
+
+const stderr_print = main.stderr_print;
+const Configuration = main.Configuration;
+
+const WorkPool = @import("./work_pool/work_pool.zig").WorkPool;
+const IgnoreTracker = @import("./gitignore.zig").IgnoreTracker;
+
+fn less_than(_: @TypeOf(.{}), a: analysis.SourceCodeFault, b: analysis.SourceCodeFault) bool {
+    return a.line_number < b.line_number;
+}
+
+fn RwLockedInteger(comptime T: type) type {
+    return struct {
+        value: T,
+        lock: std.Thread.RwLock,
+
+        fn init(value: T) RwLockedInteger(T) {
+            return RwLockedInteger(T){
+                .value = value,
+                .lock = std.Thread.RwLock {},
+            };
+        }
+
+        fn read(self: *const RwLockedInteger(T)) T {
+            return self.value;
+        }
+
+        fn add(self: *RwLockedInteger(T), to_add: T) void {
+            self.lock.lock();
+            defer self.lock.unlock();
+            self.value += to_add;
+        }
+    };
+}
+
+const WorkerThreadContext = struct {
+    analyzer: *const analysis.ASTAnalyzer,
+    configuration: *const Configuration,
+    shared_error_counter: *RwLockedInteger(u64),
+    allocator: std.mem.Allocator,
+    file_metadata: *const std.fs.File.Metadata,
+    file_name: []const u8,
+};
+
+pub const ParallelLinter = struct {
+    analyzer: *const analysis.ASTAnalyzer,
+    configuration: *const Configuration,
+    // this maybe could be an atomic?
+    // but Zig's std.atomic.Atomic isn't well documented and I don't want to make a mistake
+    shared_error_counter: RwLockedInteger(u64),
+    /// Non-arena allocator for things like files. Only for the main process.
+    allocator: std.mem.Allocator,
+    /// Thread-safe allocator that threads can use.
+    thread_safe_allocator: std.heap.ThreadSafeAllocator,
+    /// tracks seen files; never used in threads
+    seen: std.StringHashMap(void),
+    /// tracks files to ignore; never used in threads
+    ignore_tracker: *const IgnoreTracker,
+
+    /// Initializes a new ParallelLinter with a particular analysis configuration.
+    ///
+    /// Runs in the main process.
+    pub fn init(
+        arena_allocator: std.mem.Allocator,
+        normal_allocator: std.mem.Allocator,
+        analyzer: *const analysis.ASTAnalyzer,
+        configuration: *const Configuration,
+        ignore_tracker: *const IgnoreTracker,
+    ) ParallelLinter {
+
+        return .{
+            .analyzer = analyzer,
+            .configuration = configuration,
+            .shared_error_counter = RwLockedInteger(u64).init(0),
+            .seen = std.StringHashMap(void).init(arena_allocator),
+            .allocator = normal_allocator,
+            .thread_safe_allocator = std.heap.ThreadSafeAllocator {
+                .child_allocator = normal_allocator,
+            },
+            .ignore_tracker = ignore_tracker,
+        };
+    }
+
+    /// Runs the linter on a particular file/directory, setting up threads and all.
+    /// Returns the number of error-severity faults found.
+    ///
+    /// Runs in the main process.
+    pub fn run(self: *ParallelLinter, file_or_directory: []const u8) !u64 {
+        // iterate over the file/directory, sending files we should lint to the WorkPool
+        // TODO: should there be a case where if we're not given a directory we skip the thread stuff for performance?
+        try self.handle_file_or_directory(file_or_directory, false);
+
+        // finally, wait for the work pool to be done and return the # of errors
+        // std.debug.print("about to deinit WorkPool...\n", .{});
+        // try WorkPool.waitAndDeinit();
+        return self.shared_error_counter.read();
+    }
+
+    /// Handles a file or directory, putting files to lint on the queue.
+    ///
+    /// Runs in the main process.
+    fn handle_file_or_directory(
+        self: *ParallelLinter,
+        file_or_directory: []const u8,
+        is_top_level: bool,
+    ) !void {
+        // we need the full, not relative, path to make sure we avoid symlink loops
+        const real_path = try std.fs.realpathAlloc(self.allocator, file_or_directory);
+        if (self.seen.contains(real_path)) {
+            // we need to free the `real_path` memory here since we're not adding it to the hashmap
+            self.allocator.free(real_path);
+            return;
+        } else {
+            try self.seen.put(real_path, {});
+        }
+
+        const file = std.fs.cwd().openFile(file_or_directory, .{}) catch |err| {
+            switch (err) {
+                error.AccessDenied => try stderr_print("error: access denied: '{s}'", .{file_or_directory}),
+                error.DeviceBusy => try stderr_print("error: device busy: '{s}'", .{file_or_directory}),
+                error.FileNotFound => try stderr_print("error: file not found: '{s}'", .{file_or_directory}),
+                error.FileTooBig => try stderr_print("error: file too big: '{s}'", .{file_or_directory}),
+
+                error.SymLinkLoop => {
+                    // symlink loops should be caught by our hashmap of seen files
+                    // if not, we have a problem, so let's check
+                    if (!self.seen.contains(real_path)) {
+                        try stderr_print(
+                            "error: couldn't open '{s}' due to a symlink loop, " ++
+                                "but it still hasn't been linted (full path: {s})",
+                            .{ file_or_directory, real_path },
+                        );
+                    }
+                },
+
+                // Windows can't open a directory with openFile, apparently.
+                error.IsDir => return try self.handle_directory(file_or_directory),
+
+                else => try stderr_print("error: couldn't open '{s}': {}", .{ file_or_directory, err }),
+            }
+
+            // exit the program if the *user* specified an inaccessible file;
+            // otherwise, just skip it
+            if (is_top_level) {
+                std.os.exit(1);
+            } else {
+                return;
+            }
+        };
+        defer file.close();
+
+        const metadata = try file.metadata(); // TODO: is .stat() faster?
+        const kind = metadata.kind();
+        switch (kind) {
+            .file => {
+                if (!is_top_level) {
+                    // not a Zig file + not directly specified by the user
+                    if (!std.mem.endsWith(u8, file_or_directory, ".zig")) return;
+                    // ignored by a .gitignore
+                    if (try self.ignore_tracker.is_ignored(file_or_directory)) return;
+                }
+
+                // send it to the work pool
+                const threadsafe_allocator = self.thread_safe_allocator.allocator();
+                const file_name_for_thread = try threadsafe_allocator.alloc(u8, file_or_directory.len);
+                @memcpy(file_name_for_thread, file_or_directory);
+
+                std.debug.print("at go\n",.{});
+                try WorkPool.go(
+                    self.allocator,
+                    WorkerThreadContext,
+                    WorkerThreadContext {
+                        .analyzer = self.analyzer,
+                        .configuration = self.configuration,
+                        .shared_error_counter = &self.shared_error_counter,
+                        .allocator = threadsafe_allocator,
+                        .file_metadata = &metadata,
+                        .file_name = file_name_for_thread,
+                    },
+                    ParallelLinter.worker_thread,
+                );
+            },
+            .directory => try self.handle_directory(file_or_directory),
+            else => {
+                try stderr_print(
+                    "ignoring '{s}', which is not a file or directory, but a(n) {}.",
+                    .{ file_or_directory, kind },
+                );
+            },
+        }
+    }
+
+    /// This is its own helper function because of W*ndows.
+    fn handle_directory(self: *ParallelLinter, directory_name: []const u8) anyerror!void {
+        var dir = try std.fs.cwd().openIterableDir(directory_name, .{});
+        defer dir.close();
+
+        var iterable = dir.iterate();
+        var entry = try iterable.next();
+        while (entry != null) : (entry = try iterable.next()) {
+            const full_name = try std.fs.path.join(self.allocator, &[_][]const u8{ directory_name, entry.?.name });
+            defer self.allocator.free(full_name);
+            try self.handle_file_or_directory(full_name, false);
+        }
+    }
+
+    /// The function that runs within each thread in the worker pool
+    fn worker_thread(context: WorkerThreadContext) void {
+        // ahora estamos en la thread
+        var error_count: u64 = 0;
+
+        std.debug.print("IN THE THREAD! file_name = {s}\n", .{context.file_name});
+        const file = std.fs.cwd().openFile(context.file_name, .{}) catch |err| {
+            stderr_print("error: couldn't open '{s}': {}", .{ context.file_name, err }) catch unreachable;
+            return;
+        };
+        defer file.close();
+
+        const contents = context.allocator.allocSentinel(u8, context.file_metadata.size(), 0) catch |err| {
+            stderr_print("error: couldn't allocate memory to lint '{s}': {}", .{ context.file_name, err }) catch unreachable;
+            return;
+        };
+        defer context.allocator.free(contents);
+
+        _ = file.readAll(contents) catch |err| {
+            stderr_print("error: couldn't read '{s}': {}", .{ context.file_name, err }) catch unreachable;
+            return;
+        };
+
+        var ast = std.zig.Ast.parse(context.allocator, contents, .zig) catch |err| {
+            stderr_print("error: couldn't parse '{s}' into Zig AST: {}", .{ context.file_name, err }) catch unreachable;
+            return;
+        };
+        defer ast.deinit(context.allocator);
+
+        var faults = context.analyzer.analyze(context.allocator, context.file_name, ast) catch |err| {
+            stderr_print("error: couldn't analyze '{s}': {}", .{ context.file_name, err }) catch unreachable;
+            return;
+        };
+        defer faults.deinit();
+
+        // TODO just return faults.items
+
+        const sorted_faults = std.sort.insertion(analysis.SourceCodeFault, faults.faults.items, .{}, less_than);
+        _ = sorted_faults;
+        const stdout = std.io.getStdOut();
+        const stdout_writer = stdout.writer();
+
+        const use_color: bool = stdout.supportsAnsiEscapeCodes();
+        const bold_text: []const u8 = if (use_color) "\x1b[1m" else "";
+        const red_text: []const u8 = if (use_color) "\x1b[31m" else "";
+        const yellow_text: []const u8 = if (use_color) "\x1b[33m" else "";
+
+        // currently not used but makes for a nice highlight!
+        // const bold_magenta: []const u8 = if (use_color) "\x1b[1;35m" else "";
+        const end_text_fmt: []const u8 = if (use_color) "\x1b[0m" else "";
+        for (faults.faults.items) |fault| {
+            stdout_writer.print("{s}{s}:{}:{}{s}: ", .{
+                bold_text,
+                context.file_name,
+                fault.line_number,
+                fault.column_number,
+                end_text_fmt,
+            }) catch unreachable;
+            var warning = false;
+            var fault_formatting = red_text;
+            switch (fault.fault_type) {
+                .LineTooLong => |len| {
+                    if (
+                        context.configuration.max_line_length != null and
+                        context.configuration.max_line_length.?.severity == .Warning
+                    ) {
+                        warning = true;
+                        fault_formatting = yellow_text;
+                    }
+
+                    stdout_writer.print(
+                        "line is {s}{} characters long{s}; the maximum is {}",
+                        .{ fault_formatting, len, end_text_fmt, context.analyzer.max_line_length },
+                    ) catch unreachable;
+                },
+                .BannedCommentPhrase => |phrase_info| {
+                    if (phrase_info.severity_level == .Warning) {
+                        warning = true;
+                        fault_formatting = yellow_text;
+                    }
+
+                    stdout_writer.print(
+                        "comment includes banned phrase '{s}{s}{s}':\n    {s}=> {s}{s}",
+                        .{
+                            fault_formatting, phrase_info.phrase, end_text_fmt,
+                            fault_formatting, end_text_fmt,       phrase_info.comment,
+                        },
+                    ) catch unreachable;
+                },
+                .DupeImport => |name| {
+                    // TODO: can we do some shenanigans with @field to make this if not have to be in every switch?
+                    if (context.configuration.dupe_import == .Warning) {
+                        warning = true;
+                        fault_formatting = yellow_text;
+                    }
+
+                    stdout_writer.print(
+                        "found {s}duplicate import{s} of {s}",
+                        .{ fault_formatting, end_text_fmt, name },
+                    ) catch unreachable;
+                },
+                .FileAsStruct => |capitalize| {
+                    if (context.configuration.file_as_struct == .Warning) {
+                        warning = true;
+                        fault_formatting = yellow_text;
+                    }
+
+                    if (capitalize) {
+                        stdout_writer.print(
+                            "found top level fields, file name should be {s}capitalized{s}",
+                            .{ fault_formatting, end_text_fmt },
+                        ) catch unreachable;
+                    } else {
+                        stdout_writer.print(
+                            "found no top level fields, file name should be {s}lowercase{s}",
+                            .{ fault_formatting, end_text_fmt },
+                        ) catch unreachable;
+                    }
+                },
+                .ImproperlyFormatted => {
+                    if (context.configuration.check_format == .Warning) {
+                        warning = true;
+                        fault_formatting = yellow_text;
+                    }
+                    stdout_writer.print(
+                        "the file is {s}improperly formatted{s}; try using `zig fmt` to fix it",
+                        .{ fault_formatting, end_text_fmt },
+                    ) catch unreachable;
+                },
+                .ASTError => {
+                    stdout_writer.print("Zig's code parser detected an error: {s}", .{red_text}) catch unreachable;
+                    ast.renderError(fault.ast_error.?, stdout_writer) catch |err| {
+                        stderr_print("error: couldn't render AST error for {s}: {}", .{ context.file_name, err }) catch unreachable;
+                    };
+                    stdout_writer.print("{s}", .{end_text_fmt}) catch unreachable;
+                },
+            }
+            stdout_writer.writeAll("\n") catch unreachable;
+
+            if (!warning) error_count += 1;
+        }
+
+        // increment *global* error count
+        if (error_count != 0) {
+            context.shared_error_counter.add(error_count);
+        }
+        std.debug.print("reached end of worker_thread\n", .{});
+        return;
+    }
+};
